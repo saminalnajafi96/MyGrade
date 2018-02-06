6034251a313837a9d85c35150a2a5f9825c47e34
Samin Alnajafi <saminalnajafi96@gmail.com>
2018-02-06 19:47:18 +0000

    Moved module-related functions to Module class

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main
    {
       
-      private static Scanner scan = new Scanner(System.in);
+      public static Scanner scan = new Scanner(System.in);
       
       public static void main(String[] args)
          {
@@ -13,10 +13,11 @@ public class Main
             student.setStudentName(scan.nextLine());
             
             System.out.println("Hello, " + student.getStudentName() + ". Now let's create a module.");
-            createModule();
-            System.out.println("Great, here's what you've added: ");
             
-            Module.printModuleArrl();
+            student.createModule();
+            
+            System.out.println("Great, here's what you've added: ");
+            Module.printModules();
             
             boolean exit = false;
             int choice;
@@ -34,24 +35,11 @@ public class Main
                   choice = scan.nextInt();
                   scan.nextLine();
                   
-                  if(choice == 1)
-                     {
-                        createModule();
-                     }
-                  if(choice == 2)
-                     {
-                        editModule();
-                     }
-                  if(choice == 3)
-                     {
-                        deleteModule();
-                     }
-                  if(choice == 4)
-                     {
-                        Module.printModuleArrl();
-                     }
-                  if(choice == 5)
-                     {/*Calculate grade to be completed*/}
+                  if(choice == 1) {student.createModule();}
+                  if(choice == 2) {student.editModule();}
+                  if(choice == 3) {student.deleteModule();}
+                  if(choice == 4) {student.printModules();}
+                  if(choice == 5) {/*Calculate grade to be completed*/ calculateGrade();}
                   if(choice == 6)
                      {
                         System.out.println("WARNING: Nothing will be saved after exiting. Are you sure you want to exit? (y/n)");
@@ -59,150 +47,27 @@ public class Main
                         if(answer.equals("y"))
                            {
                               exit = true;
-                              
                            }
                         else
                            {
                               continue;
                            }
                      }
-                  if(choice < 1 || choice > 6){
-                     System.out.println("Sorry that's not an option");
-                  }
+                  if(choice < 1 || choice > 6){System.out.println("Sorry that's not an option");}
                }
             scan.close();
             System.out.println("Goodbye.");
             System.exit(0);
          }
-      
-      /**
-       * This function creates a new module and adds it to the list of other created modules
-       *
-       * @return a succesfully created module
-       */
-      public static Module createModule()
-         {
-            Module module = new Module();
-            
-            System.out.println("Module name:");
-            module.setModuleName(scan.nextLine());
-            
-            System.out.println("Credits:");
-            module.setCredits(scan.nextInt());
-            
-            System.out.println("Score:");
-            module.setScore(scan.nextInt());
-            
-            module.addToModuleArrl(module);
-            
-            return module;
-         }
-      
-      /**
-       * This function deletes a selected module
-       */
-      public static void deleteModule()
-         {
-            if(Module.moduleArrl.size() < 1)
-               {
-                  System.out.println("There are no modules to delete!");
-                  return;
-               }
    
-            boolean isValid = false;
-            int moduleToDelete;
-            
-            while(!isValid){
-               System.out.println("Here are the modules you've currently created");
-               Module.printModuleArrl();
-   
-               System.out.println("Which one would you like to delete (enter the corresponding module number)");
-               moduleToDelete = scan.nextInt();
-               
-               if(moduleToDelete > Module.moduleArrl.size() || moduleToDelete <=0){
-                  System.out.println("Sorry, that's not an option. Please choose one of the modules");
-               }
-               else{
-                  moduleToDelete--;
-                  Module.moduleArrl.remove(moduleToDelete);
-                  System.out.println("Module " + (moduleToDelete+1) + " successfully deleted.");
-                  isValid = true;
-               }
-            }
-         }
-      
       /**
-       * This function edits the details of a selected module
+       * This function calculates the overall grade based off the modules
+       * @return overall grade
        */
-      public static void editModule()
-         {
-            if(Module.moduleArrl.size() < 1)
-               {
-                  System.out.println("There are no modules to edit!");
-                  return;
-               }
-            boolean isValid = false;
-            int moduleToEdit = 0;
-            try
-               {
-                  while(!isValid)
-                     {
-                        System.out.println("Here are the modules you've currently created");
-                        Module.printModuleArrl();
-                        System.out.println("Which one would you like to edit? (enter the corresponding module number)");
-                        
-                        moduleToEdit = scan.nextInt();
-                        if(moduleToEdit > Module.moduleArrl.size() || moduleToEdit <= 0)
-                           {
-                              System.out.println("Sorry, that's not an option. Please choose one of the modules.");
-                           }
-                        else {
-                           moduleToEdit--;
-                           isValid = true;
-                        }
-                     }
-                  
-                  isValid = false;
-                  
-                  while(!isValid){
-                     System.out.println("What would you like to edit?" + "\n" +
-                             "1. Module name" + "\n" +
-                             "2. Credits" + "\n" +
-                             "3. Score");
-                     int choice = scan.nextInt();
-                     
-                     if(choice == 1)
-                        {
-                           System.out.println("What would you like to change the name to?");
-                           scan.nextLine();
-                           String newModuleName = scan.nextLine();
-                           Module.moduleArrl.get(moduleToEdit).setModuleName(newModuleName);
-                           isValid = true;
-                        }
-                     if(choice == 2)
-                        {
-                           System.out.println("What would you like to change the credits to?");
-                           scan.nextLine();
-                           int newCredits = scan.nextInt();
-                           Module.moduleArrl.get(moduleToEdit).setCredits(newCredits);
-                           isValid = true;
-                        }
-                     if(choice==3)
-                        {
-                           System.out.println("What would you like to change the score to?");
-                           scan.nextLine();
-                           int newScore = scan.nextInt();
-                           Module.moduleArrl.get(moduleToEdit).setScore(newScore);
-                           isValid = true;
-                        }
-                     else{
-                        System.out.println("Sorry, that's not an option.");
-                     }
-                  }
-               }
-            catch(Exception e)
-               {
-                  System.out.println("Sorry, that's not an option");
-               }
+      public static int calculateGrade(){
+         for(int i = 0; i < Module.moduleArrl.size();i++){
+            
          }
+         return 0;
+      }
    }
--- src/Module.java
+++ src/Module.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
  */
 public class Module
    {
-      
       public static ArrayList<Module> moduleArrl = new ArrayList<>();
       private String moduleName, grade;
       private int credits, totalCredits, score;
@@ -20,7 +19,7 @@ public class Module
       /**
        * Prints the contents of modules list
        */
-      public static void printModuleArrl(){
+      public static void printModules(){
          if(moduleArrl.size() < 1){
             System.out.println("There are no modules to print!");
             return;
@@ -40,6 +39,142 @@ public class Module
       public void addToModuleArrl(Module module){
          moduleArrl.add(module);
       }
+   
+      /**
+       * This function creates a new module and adds it to the list of other created modules
+       *
+       * @return a succesfully created module
+       */
+      public void createModule()
+         {
+            if(totalCredits == 120){
+               System.out.println("Sorry, there is a maximum of 120 credits. Check if you've entered module details " +
+                       "correctly");
+               return;
+            }
+            
+            Module module = new Module();
+            
+            System.out.println("Module name:");
+            module.setModuleName(Main.scan.nextLine());
+         
+            System.out.println("Credits:");
+            module.setCredits(Main.scan.nextInt());
+            totalCredits += module.getCredits();
+         
+            System.out.println("Score:");
+            module.setScore(Main.scan.nextInt());
+         
+            addToModuleArrl(module);
+         }
+   
+      /**
+       * This function deletes a selected module
+       */
+      public static void deleteModule()
+         {
+            if(moduleArrl.size() < 1)
+               {
+                  System.out.println("There are no modules to delete!");
+                  return;
+               }
+         
+            boolean isValid = false;
+            int moduleToDelete;
+         
+            while(!isValid){
+               System.out.println("Here are the modules you've currently created");
+               printModules();
+            
+               System.out.println("Which one would you like to delete (enter the corresponding module number)");
+               moduleToDelete = Main.scan.nextInt();
+            
+               if(moduleToDelete > moduleArrl.size() || moduleToDelete <=0){
+                  System.out.println("Sorry, that's not an option. Please choose one of the modules");
+               }
+               else{
+                  moduleToDelete--;
+                  moduleArrl.remove(moduleToDelete);
+                  System.out.println("Module " + (moduleToDelete+1) + " successfully deleted.");
+                  isValid = true;
+               }
+            }
+         }
+   
+      /**
+       * This function edits the details of a selected module
+       */
+      public static void editModule()
+         {
+            if(Module.moduleArrl.size() < 1)
+               {
+                  System.out.println("There are no modules to edit!");
+                  return;
+               }
+            boolean isValid = false;
+            int moduleToEdit = 0;
+            try
+               {
+                  while(!isValid)
+                     {
+                        System.out.println("Here are the modules you've currently created");
+                        printModules();
+                        System.out.println("Which one would you like to edit? (enter the corresponding module number)");
+                     
+                        moduleToEdit = Main.scan.nextInt();
+                        if(moduleToEdit > moduleArrl.size() || moduleToEdit <= 0)
+                           {
+                              System.out.println("Sorry, that's not an option. Please choose one of the modules.");
+                           }
+                        else {
+                           moduleToEdit--;
+                           isValid = true;
+                        }
+                     }
+               
+                  isValid = false;
+               
+                  while(!isValid){
+                     System.out.println("What would you like to edit?" + "\n" +
+                             "1. Module name" + "\n" +
+                             "2. Credits" + "\n" +
+                             "3. Score");
+                     int choice = Main.scan.nextInt();
+                  
+                     if(choice == 1)
+                        {
+                           System.out.println("What would you like to change the name to?");
+                           Main.scan.nextLine();
+                           String newModuleName = Main.scan.nextLine();
+                           moduleArrl.get(moduleToEdit).setModuleName(newModuleName);
+                           isValid = true;
+                        }
+                     if(choice == 2)
+                        {
+                           System.out.println("What would you like to change the credits to?");
+                           Main.scan.nextLine();
+                           int newCredits = Main.scan.nextInt();
+                           moduleArrl.get(moduleToEdit).setCredits(newCredits);
+                           isValid = true;
+                        }
+                     if(choice==3)
+                        {
+                           System.out.println("What would you like to change the score to?");
+                           Main.scan.nextLine();
+                           int newScore = Main.scan.nextInt();
+                           moduleArrl.get(moduleToEdit).setScore(newScore);
+                           isValid = true;
+                        }
+                     else{
+                        System.out.println("Sorry, that's not an option.");
+                     }
+                  }
+               }
+            catch(Exception e)
+               {
+                  System.out.println("Sorry, that's not an option");
+               }
+         }
       
       @Override
       public String toString(){
@@ -56,6 +191,8 @@ public class Module
       public int getCredits(){
          return credits;
       }
+      
+      public int getTotalCredits(){return totalCredits;}
       
       public int getScore(){
          return score;
